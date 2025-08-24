85fcfa342ab6f3db0167470db967446218c1235f
aabert <aabert@smail.uni-koeln.de>
2025-08-24 13:24:26 +0200

    implemented theme change

--- js/schottland.js
+++ js/schottland.js
@@ -70,8 +70,8 @@ function vorschlagEinsenden() {
   post += "<h3>" + document.getElementById("stadt").value + "  | " + rating + "</h3> <p>";
   post += (document.getElementById("name").value == "") ? "" : ("Vorschlag von " + document.getElementById("name").value);
   post += (document.getElementById("datum").value == "") ? "" : (" am " + document.getElementById("datum").value);
-  post += "</p> </div>"  + document.getElementById("beschreibung").value  + "</article>";
-  if (document.getElementById("nutzervorschlaege").innerHTML =="Noch keine Vorschläge") {
+  post += "</p> </div>" + document.getElementById("beschreibung").value + "</article>";
+  if (document.getElementById("nutzervorschlaege").innerHTML == "Noch keine Vorschläge") {
     document.getElementById("nutzervorschlaege").innerHTML = post;
   } else {
     document.getElementById("nutzervorschlaege").innerHTML += post;
@@ -81,22 +81,79 @@ function vorschlagEinsenden() {
 function evaluateRating() {
   let rating;
   if (document.getElementById("1stern").checked) {
-    rating= "⭐"
+    rating = "⭐"
   } else if (document.getElementById("2sterne").checked) {
-    rating="⭐⭐"
+    rating = "⭐⭐"
   } else if (document.getElementById("3sterne").checked) {
-    rating="⭐⭐⭐"
+    rating = "⭐⭐⭐"
   } else if (document.getElementById("4sterne").checked) {
-    rating="⭐⭐⭐⭐"
+    rating = "⭐⭐⭐⭐"
   } else if (document.getElementById("5sterne").checked) {
-    rating="⭐⭐⭐⭐⭐"
+    rating = "⭐⭐⭐⭐⭐"
   }
   return rating;
 }
 
 function changeFontSize(val) {
   let paragraphs = document.getElementsByClassName("changable");
-  for (let i =0; i<paragraphs.length; i++) {
+  for (let i = 0; i < paragraphs.length; i++) {
     paragraphs[i].style.fontSize = val + "px";
   }
+}
+
+
+function changeTheme(theme) {
+  let background;
+  let text;
+  let border;
+  let tiles;
+  switch (theme) {
+    case "light":
+      background = "#cccbd4ff";
+      text = "#000000";
+      border = "#000000";
+      tiles = "#807f8aff";
+      document.documentElement.style.setProperty('--color1', '#111fa1ff');
+      document.documentElement.style.setProperty('--color2', '#ffffffff');
+      document.documentElement.style.setProperty('--color3', '#807f8aff');
+      break;
+    case "dark":
+      background = "#041644";
+      text = "white";
+      border = "#ffffffff";
+      tiles = "#041644";
+      document.documentElement.style.setProperty('--color1', 'cyan');
+      document.documentElement.style.setProperty('--color2', 'black');
+      document.documentElement.style.setProperty('--color3', '#020522');
+      break;
+    default:
+      break;
+  }
+  let boxes = document.getElementsByClassName("leftBox");
+  let sidebar = document.getElementsByClassName("sidebar");
+  let leftFlexBoxes = document.getElementsByClassName("leftBoxFlex");
+  let navCats = document.getElementsByClassName("navCat");
+  let flexItems = document.getElementsByClassName("flexItem");
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].style.backgroundColor = background;
+    boxes[i].style.color = text;
+  }
+  for (let i = 0; i < sidebar.length; i++) {
+    sidebar[i].style.backgroundColor = background;
+    sidebar[i].style.color = text;
+  }
+  for (let i = 0; i < leftFlexBoxes.length; i++) {
+    leftFlexBoxes[i].style.backgroundColor = background;
+    leftFlexBoxes[i].style.color = text;
+  }
+  for (let i = 0; i < navCats.length; i++) {
+    navCats[i].style.borderColor = border;
+    navCats[i].style.color = text;
+  }
+  for (let i = 0; i < flexItems.length; i++) {
+    flexItems[i].style.backgroundColor = tiles;
+    flexItems[i].style.borderColor = border;
+  }
+  document.body.style.color = text;
+  document.body.style.backgroundColor = background;
 }
